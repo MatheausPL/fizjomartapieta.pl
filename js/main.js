96d6335c46cf4bb682247f27002f9af99265c569
MatheausPL <mateusz.pieta.prv@gmail.com>
2026-07-28 12:33:14 +0200

    version 1.1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 const navToggle = document.querySelector('.nav__toggle');
 const navList = document.querySelector('.nav__list');
 
-navToggle.addEventListener('click', () => {
-    navList.classList.toggle('nav--open');
+if (navToggle && navList) {
+    navToggle.addEventListener('click', () => {
+        navList.classList.toggle('nav--open');
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll(".sidebar__nav a");
+    const current = window.location.pathname.split("/").pop();
+
+    links.forEach(link => {
+        if (link.getAttribute("href") === current) {
+            link.classList.add("active");
+        }
+    });
 });
